f705eb4b6f92a855e4d33b277425d8d0db6c79e2
Daniel Nataren <danielflonat@gmail.com>
2025-05-22 16:49:48 -0600

    first commit

---
+++ src/test/java/com/danielflores/springbase/SpringBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielflores.springbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
